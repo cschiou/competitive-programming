d63321d682d6685e262d4aa5ffd091bc7a4e3bd3
cschiou <cschiou08@gmail.com>
2025-07-10 21:10:57 +0800

    Create disjoint_set_union.cpp

---
+++ disjoint_set_union.cpp
@@ -0,0 +1,21 @@
+class DSU {
+  public:
+    DSU(int numOfNodes) {
+        n = numOfNodes;
+        par.resize(n);
+        iota(par.begin(), par.end(), 0);
+    }
+    int find(int a){
+        if(par[a] == a) return a;
+        par[a] = find(par[a]);
+        return par[a];
+    }
+    void join(int a, int b){
+        int pa = find(a);
+        int pb = find(b);
+        if(pa == pb) return;
+        par[pa] = par[pb] = min(pa,pb);
+    }
+    vector<int> par;
+    int n;
+};
